d97d3af1ff13b9fa31e0ca3a6a4ac2cb0a6116a9
daniloeberli <danilo.eberli@gmail.com>
2023-02-28 14:55:25 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const list = ['latte','uova','birra','carne','pomodori'];
+const ul = document.getElementById('list');
+let i = 0;
+
+while(i < list.length){
+    const listItem = document.createElement('li'); 
+    listItem.append(list[i]);
+    ul.append(listItem);
+    i++;
+}
